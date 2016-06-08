e3a2ae757faade99d57cf0f98943244a505314bd
Serhat Can <can.srht@gmail.com>
2016-06-08 22:40:38 +0300

    rename operations file

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,30 +25,15 @@ module.exports = function (grunt) {
                 src: 'test/*.js'
             }
         }
-        /*,
-         jsdoc: {
-         dist: {
-         src: ['lib/!*'],
-         jsdoc: './node_modules/.bin/jsdoc',
-         options: {
-         destination: 'docs/jsdoc/',
-         configure: './node_modules/jsdoc/conf.json',
-         template: './node_modules/ink-docstrap/template'
-         }
-         }
-         }
-         */
     });
 
-    // Load grunt tasks from npm packages
     grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks('grunt-simple-mocha');
-    // grunt.loadNpmTasks('grunt-jsdoc');
 
     // Test task
     grunt.registerTask("test", ["simplemocha"]);
 
-    // Default task.
+    // Default task
     grunt.registerTask("default", ["jshint", "simplemocha"]);
 
 };
