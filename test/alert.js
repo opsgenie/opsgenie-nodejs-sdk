a848b473f2965f5c0b4a41ea8fb5e9fac0513b55
Serhat Can <can.srht@gmail.com>
2016-06-12 02:11:37 +0300

    add new methods to Alert api and update alert api test

--- test/alert.js
+++ test/alert.js
@@ -7,19 +7,23 @@ var waterfall = require("async/waterfall");
 var opsgenie = require('../');
 require('./configure');
 
+/**
+ * As this some of these tests contain methods need to be executed in sync way, async lib's waterfall function is used
+ * to avoid callback hell problem
+ */
 describe('ALERT_API_TEST', function () {
 
+    //this.timeout(60000);
+
     // use NOCK_OFF=true mocha -t 60000 for testing against real server
-    // for HTTP Server mocking
     if (process.env.NOCK_OFF !== 'true') {
         require('./mocks/alert');
+    } else {
+
     }
 
     var timeInMs = Date.now();
 
-    /**
-     * covers create, get actions
-     */
     it('create and get success', function (done) {
 
         var create_alert_json = {
@@ -39,9 +43,6 @@ describe('ALERT_API_TEST', function () {
         });
     });
 
-    /**
-     * covers create, get and close actions
-     */
     it('create and close success', function (done) {
 
         var create_alert_json = {
@@ -79,9 +80,6 @@ describe('ALERT_API_TEST', function () {
         });
     });
 
-    /**
-     * covers create, acknowledge
-     */
     it('create and acknowledge success', function (done) {
 
         var create_alert_json = {
@@ -89,8 +87,8 @@ describe('ALERT_API_TEST', function () {
             "tags": ["tag2"],
             "description": "This is the description of the Acknowledge and list alert notes test",
             "details": {"prop1": "prop1Value", "prop2": "prop2Value"},
-            "note": "A Note for testing note listing"
         };
+
         opsgenie.alert.create(create_alert_json, function (error, alertCreateSuccess) {
             expect(error).equal(null);
             expect(alertCreateSuccess.httpStatusCode).to.equal(200);
@@ -103,5 +101,95 @@ describe('ALERT_API_TEST', function () {
         });
     });
 
-    // nock.recorder.rec();
+    it('create, add note, list notes, list alert logs success', function (done) {
+
+        var create_alert_json = {
+            "message": "create, add note, list notes success",
+            "tags": ["listTag", "logTag"],
+            "description": "This is the description of the create, add note, list notes success test",
+            "details": {
+                "importance": "critic",
+                "department": "operations"
+            },
+            "note": "A Note for testing note listing"
+        };
+
+        waterfall([
+            function (callback) {
+                opsgenie.alert.create(create_alert_json, function (error, alertCreateSuccess) {
+                    expect(error).equal(null);
+                    expect(alertCreateSuccess.httpStatusCode).to.equal(200);
+                    callback(null, alertCreateSuccess);
+                });
+            },
+            function (alertCreateSuccess, callback) {
+                var add_note_json = {
+                    "id": alertCreateSuccess.alertId,
+                    "note": "custom note"
+                };
+
+                opsgenie.alert.addNote(add_note_json, function (error, addNoteSuccess) {
+                    expect(error).equal(null);
+                    expect(addNoteSuccess.code).to.equal(200);
+                    callback(null, alertCreateSuccess.alertId);
+                });
+            },
+            function (alertId, callback) {
+                opsgenie.alert.listAlertNotes({id: alertId}, function (error, listAlertNotesSuccess) {
+                    expect(error).equal(null);
+                    expect(listAlertNotesSuccess.notes.length).to.equal(2);
+                    callback(null, alertId);
+                });
+            },
+            function (alertId, callback) {
+                opsgenie.alert.listAlertLogs({id: alertId}, function (error, listAlertLogsSuccess) {
+                    expect(error).equal(null);
+                    expect(listAlertLogsSuccess.logs.length).to.equal(3);
+                    callback(null, 'done');
+                });
+            }
+        ], function (err, result) {
+            expect(err).equal(null);
+            if (result === "done") {
+                done();
+            }
+        });
+
+    });
+
+    /**
+     * this test is useful when executed against real service. It cleans up all created alerts
+     */
+    if (process.env.NOCK_OFF === 'true') {
+        it('count, list and delete all alerts created until now', function (done) {
+            opsgenie.alert.count({createdAfter: timeInMs * 1000000}, function (error, countAlerts) {
+                expect(error).equal(null);
+                var alertCount = countAlerts.count;
+
+                opsgenie.alert.list({createdAfter: timeInMs * 1000000}, function (error, listAlerts) {
+                    expect(error).equal(null);
+                    var alerts = listAlerts.alerts;
+                    expect(alerts.length).to.equal(alertCount);
+
+                    var deleteSuccess = function (error, deleteResult) {
+                        expect(error).equal(null);
+                        expect(deleteResult.code).to.equal(200);
+                        doneCount++;
+
+                        if (doneCount === alertCount) {
+                            done();
+                        }
+                    };
+
+                    var doneCount = 0;
+                    for (var i = 0; i < alerts.length; i++) {
+                        opsgenie.alert.deleteById(alerts[i].id, deleteSuccess);
+                    }
+
+                });
+            });
+        });
+    }
+
+    //nock.recorder.rec();
 });
