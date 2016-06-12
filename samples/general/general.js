3afa597af0b74eb3a45a02919ae5d85767522001
Serhat Can <serhat@opsgenie.com>
2016-06-12 23:06:48 +0300

    update readme

--- samples/general/general.js
+++ samples/general/general.js
@@ -6,11 +6,10 @@ opsgenie.configure({
     'api_key': 'd1871497-38bc-4222-9063-32062fc6f2f4'
 });
 
-opsgenie.alert.get({id: "41fbe997-03cc-40ff-a4c4-7674b86b1490"}, function (error, alerts) {
+opsgenie.alert.get({id: "41fbe997-03cc-40ff-a4c4-7674b86b1490"}, function (error, alert) {
     if (error) {
         console.error(error);
     } else {
-        console.log("Get Alert Response");
-        console.log(alerts);
+        console.log("Alert Response: ", alert);
     }
 });
