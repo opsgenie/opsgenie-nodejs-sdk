ae4354a9db581a2e7ed165ddeddb168228eec353
Serhat Can <serhat@opsgenie.com>
2016-06-18 17:48:08 +0300

    add api.js as later replacement of client.js and add update configuration structure

--- samples/configure.js
+++ samples/configure.js
@@ -6,14 +6,12 @@ var opsgenie = require('../');
 opsgenie.configure({
     'mode': 'lab',
     'api_key': "91e58022-8be4-4590-a4f0-0d4030bd9b0d"
-    // 'api_key': api_key
 });
 
 // sample conf for localhost
 /*
 {
     'mode': 'localhost',
-    'host': 'localhost',
-    'post': '9000',
+    'host': 'localhost:9000',
     'api_key': 'local_key'
 }*/
--- samples/general/general.js
+++ samples/general/general.js
@@ -6,6 +6,11 @@ opsgenie.configure({
     'api_key': 'd1871497-38bc-4222-9063-32062fc6f2f4'
 });
 
+// optional for dev purposes
+/*opsgenie.headers({
+    
+});*/
+
 opsgenie.alert.get({id: "41fbe997-03cc-40ff-a4c4-7674b86b1490"}, function (error, alert) {
     if (error) {
         console.error(error);
